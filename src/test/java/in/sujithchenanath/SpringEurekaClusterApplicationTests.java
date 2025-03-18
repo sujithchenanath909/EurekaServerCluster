31bea9d62c74c280d47ba944e25d990239fb2c7d
Sujith Chenanath <sujithchenanath@gmail.com>
2025-03-18 20:39:06 +0530

    first commit

---
+++ src/test/java/in/sujithchenanath/SpringEurekaClusterApplicationTests.java
@@ -0,0 +1,13 @@
+package in.sujithchenanath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaClusterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
