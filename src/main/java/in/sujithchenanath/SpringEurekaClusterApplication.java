91b08a64a4a26ffc6f93a08985d5456c703ffd9f
Sujith Chenanath <sujithchenanath@gmail.com>
2025-03-18 20:51:59 +0530

    added peer profiles

--- src/main/java/in/sujithchenanath/SpringEurekaClusterApplication.java
+++ src/main/java/in/sujithchenanath/SpringEurekaClusterApplication.java
@@ -2,8 +2,11 @@ package in.sujithchenanath;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringEurekaClusterApplication {
 
 	public static void main(String[] args) {
